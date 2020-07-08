c3170878ef5435d88de0c5acdc122e55ba68684b
Yunus Emre Gül <yunemregul@gmail.com>
2020-07-08 09:16:02 +0300

    App.js deki Login bölümü Login componentine taşındı
    Tasarımda güncellemeler

--- mobil/imgpaylas/App.js
+++ mobil/imgpaylas/App.js
@@ -1,7 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
-import { StyleSheet } from "react-native";
 import Login from "./components/stacks/Login";
 
 const Stack = createStackNavigator();
@@ -20,43 +19,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F3F4F5",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  textinput: {
-    marginTop: 14,
-    color: "#39375B",
-    fontSize: 17,
-    borderColor: "#39375B",
-    width: 245,
-    height: 40,
-    borderWidth: 2,
-    padding: 10,
-    borderRadius: 2,
-  },
-  loginbutton: {
-    padding: 8,
-    backgroundColor: "#39375B",
-    marginTop: 32,
-    width: 245,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 32,
-    height: 40,
-  },
-  registerbutton: {
-    padding: 8,
-    backgroundColor: "#E76F51",
-    marginTop: 14,
-    width: 245,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 32,
-    height: 40,
-  },
-});
--- mobil/imgpaylas/components/stacks/Login.js
+++ mobil/imgpaylas/components/stacks/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { Image, Text, View, TextInput, TouchableOpacity } from "react-native";
-import styles from "../../App";
+import { Image, Text, View, TextInput, TouchableOpacity, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 export default class Login extends Component {
@@ -55,3 +54,44 @@ export default class Login extends Component {
     );
   }
 }
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#F3F4F5",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    textinput: {
+      marginTop: 14,
+      color: "#39375B",
+      fontSize: 17,
+      borderColor: "#39375B",
+      width: 245,
+      height: 40,
+      borderWidth: 2,
+      padding: 10,
+      borderRadius: 2,
+    },
+    loginbutton: {
+      padding: 8,
+      backgroundColor: "#39375B",
+      marginTop: 32,
+      width: 245,
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 32,
+      height: 40,
+    },
+    registerbutton: {
+      padding: 8,
+      backgroundColor: "#E76F51",
+      marginTop: 14,
+      width: 245,
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 32,
+      height: 40,
+    },
+  });
+  
