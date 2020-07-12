6f3330f1919527eac02f97c4ffabdfefebe4cd53
Yunus Emre Gül <yunemregul@gmail.com>
2020-07-12 16:50:50 +0300

    Firebase de database kullanmak yerine firestore ye geçildi

--- firebase/imgpaylas/functions/save-users.js
+++ firebase/imgpaylas/functions/save-users.js
@@ -1,11 +1,19 @@
-'use strict';
+"use strict";
 
-const functions = require('firebase-functions');
-const admin = require('./admin');
+const functions = require("firebase-functions");
+const admin = require("./admin");
 
 const createUser = functions.auth.user().onCreate(async (user) => {
-  await admin.database().ref("users").push({email: user.email, displayName: user.displayName, uid: user.uid});
-  return console.log("New user with email '"+user.email+"' saved successfully! ");
+  await admin
+    .firestore()
+    .collection("users")
+    .doc(user.uid)
+    .set({ email: user.email, displayName: user.displayName })
+    .then(() => {
+      console.log(
+        "New user with email '" + user.email + "' saved successfully!"
+      );
+    });
 });
 
 module.exports = createUser;
